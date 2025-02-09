4da6da0fc4683e30227dbab83cdf0d92d4b2899d
WinRAR <99762912+WINRARisyou@users.noreply.github.com>
2025-02-09 10:01:07 -0500

    Update main.js

--- files/script/main.js
+++ files/script/main.js
@@ -5,14 +5,14 @@ createWindow("Exit Path 4 (very unfinished/abandoned mod)", "Exit-Path", 600, 40
 createWindow("Exit Path 4 Level Editor", "Exit-Path-Editor", 423, 294, "./files/html/Exit Path Stage Creator.html", null, "#808080");
 createWindow("Zombie Zapper b1.8", "zombie-zapper", 640, 360, "./files/html/Zombie Zapper b1.8.html", null, "#808080");
 createWindow("Snake", "snake", 400, 400, "./files/html/snake.html", null, "#F5F5D3");
-createWindow("Sonic Mania WebGL", "sonic-mania", 640, 360, "https://winrarisyou.github.io/SonicManiaWeb/", null, "#1e43fc");
+//createWindow("Sonic Mania WebGL", "sonic-mania", 640, 360, "https://winrarisyou.github.io/SonicManiaWeb/", null, "#1e43fc");
 createWindow("Bad Piggies WebGL", "bad-piggies", 640, 360, "https://winrarisyou.github.io/bad-piggies-webgl/", null, "#fbb720");
-createWindow("Celeste WASM", "celeste-wasm", 640, 360, "https://winrarisyou.github.io/celeste-wasm/", null, "#FFB6F8");
+//createWindow("Celeste WASM", "celeste-wasm", 640, 360, "https://winrarisyou.github.io/celeste-wasm/", null, "#FFB6F8");
 showWindow("about");
 closeWindow("Exit-Path");
 closeWindow("Exit-Path-Editor");
 closeWindow("zombie-zapper");
 closeWindow("snake");
-closeWindow("sonic-mania");
+//closeWindow("sonic-mania");
 closeWindow("bad-piggies");
-closeWindow("celeste-wasm");
+//closeWindow("celeste-wasm");
